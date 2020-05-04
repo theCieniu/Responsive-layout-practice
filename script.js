a4a7b8a5ed30e6f8a308f0a2b788e75c5b22bb9e
Paulina Weiher <45978460+theCieniu@users.noreply.github.com>
2020-05-04 14:57:23 +0200

    mobile navigation
    I wish I've written this mobile first from the beginning. I might rewrite it some day

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //document.addEventListener("scroll", alert("first test"))
+const toggle = document.querySelector(".nav-toggle")
+const navigation = document.querySelector(".nav-lists")
+
+toggle.addEventListener("click", () => {
+  navigation.classList.toggle("hidden")
+})
